5de012451c917b2116a65c2a560e437fa08f9a5a
KmIndu <73270523+KmIndu@users.noreply.github.com>
2021-11-15 14:59:52 +0530

    Update duplicate.cpp

--- Week 2/duplicate.cpp
+++ Week 2/duplicate.cpp
@@ -1,77 +1,54 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int binary_Search_start(int a[], int l, int h, int key, int &start)
+void duplicates(int *a,int n,int key)
 {
-    while (l <= h)
+    int low,high,mid,first,last;
+    low=0;
+    high=n-1;
+    
+    while(low<high)
     {
-        int mid = (l + h) / 2;
-        if (a[mid] == key)
+        mid=(low+high)/2;
+        if(a[mid]==key)
         {
-            start = mid;
-            h = mid - 1;
+            int x=mid;
+            first=mid;
+            last=mid;
+            while(a[--x]==key)
+            first=x;
+            x=mid;
+            while(a[++x]==key)
+            last=x;
+            cout<<key<<" - "<<last-first+1<<endl;
+            return;
         }
-        else if (a[mid] < key)
-        {
-            l = mid + 1;
-        }
+        else if(key<a[mid])
+        high=mid-1;
         else
-        {
-            h = mid - 1;
-        }
+        low=mid+1;
     }
-    return start;
-}
-
-int binary_Search_end(int a[], int l, int h, int key, int &end)
-{
-    while (l <= h)
-    {
-        int mid = (l + h) / 2;
-        if (a[mid] == key)
-        {
-            end = mid;
-            l = mid + 1;
-        }
-        else if (a[mid] < key)
-        {
-            l = mid + 1;
-        }
-        else
-        {
-            h = mid - 1;
-        }
-    }
-    return end;
+    cout<<"Key not present\n";
 }
 
 int main()
 {
-
     int t;
-    cin >> t;
+    cin>>t;
     while (t--)
     {
-        int n, key;
-        cin >> n;
+        int n,i,key;
+
+        cin>>n;
+
         int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        cin >> key;
-        int start = -1;
-        if (binary_Search_start(a, 0, n - 1, key, start) < 0)
-        {
-            cout << "key not present"
-                 << "\n";
-        }
-        else
-        {
-            int end = -1;
-            binary_Search_end(a, 0, n - 1, key, end);
-            cout << key << " - " << end - start + 1 << "\n";
-        }
+        for(i=0;i<n;i++)
+        cin>>a[i];
+
+        cin>>key;
+
+        duplicates(a,n,key);
     }
+    
     return 0;
 }
